2a327dd50bbdebb4a1e351b117091fc1668942f3
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-10-01 15:55:45 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 20;
+let num4 = 16;
+let mod = num3 % num4;
+
+let max = Math.max(1, 3, 4, 6, 7, 9, 10, 12, 13, 15, 16, 18, 19, 20);
